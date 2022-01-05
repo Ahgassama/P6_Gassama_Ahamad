90fc54607395030886e1bba7676593844062afd1
Ahgassama <ahmad.gassama@gmail.com>
2022-01-05 21:34:57 +0100

    regex email pr création user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const userCtrl = require("../controllers/user");
 const passwordValidatorSchema = require("../controllers/password");
+const trueEmail = require("../controllers/email");
 
-router.post("/signup", passwordValidatorSchema, userCtrl.signup);
+router.post("/signup", trueEmail, passwordValidatorSchema, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
